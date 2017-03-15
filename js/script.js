afb02ea37a8560596364bac5faf92cb44bd5650e
MiguelSilva96 <a74601@alunos.uminho.pt>
2017-03-15 03:23:41 +0000

    Versao final do trabalho

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+function openList(name) {
+    var list = document.getElementById(name);
+    var style = window.getComputedStyle(list, null);
+
+    if (style.display == "none")
+        list.style.display = "block";
+    else
+        list.style.display = "none";
+}
